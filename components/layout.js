ff61ede2e94175bdfe88d2bb9fe962db4de10aeb
Billi Ramirez <ramirezbilly42@outlook.com>
2020-05-12 14:08:44 -0600

    adding nprogress

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,21 @@
 import Link from "next/link";
+import Head from "next/head";
+import Router from "next/router";
+import Nprogress from "nprogress";
+
+Router.onRouteChangeStart = (url) => Nprogress.start();
+Router.onRouteChangeComplete = () => Nprogress.done();
+Router.onRouteChangeError = () => Nprogress.done();
 
 const Layout = ({ children, title }) => (
-  <div>
+  <div className="root">
+    <Head>
+      <title>Next Portfolio</title>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
+      />
+    </Head>
     <header>
       <Link href="/">
         <a>Home</a>
@@ -15,7 +29,47 @@ const Layout = ({ children, title }) => (
     </header>
     <h1>{title}</h1>
     {children}
-    <footer>footer</footer>
+    <footer>&copy; {new Date().getFullYear()}</footer>
+    <style jsx>{`
+      .root {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      header {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        padding: 1em;
+        font-size: 1.2rem;
+        background: indigo;
+      }
+
+      header a {
+        color: darkgrey;
+        text-decoration: none;
+      }
+
+      header a:hover {
+        font-weight: bold;
+        color: lightgrey;
+      }
+
+      footer {
+        padding: 1em;
+      }
+    `}</style>
+    <style global jsx>
+      {`
+        body {
+          margin: 0;
+          font-size: 100%;
+          background: #f0f0f0;
+        }
+      `}
+    </style>
   </div>
 );
 
